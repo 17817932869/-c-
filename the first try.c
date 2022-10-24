7779325e3a289c716cb748b9e1284cd0dce18ba5
Genk <99627235+17817932869@users.noreply.github.com>
2022-10-24 23:28:18 +0800

    Add files via upload

---
+++ the first try.c
@@ -0,0 +1,15 @@
+//#include <stdio.h>
+//int main(void) { 
+//	int num = 1;//声明一个变量
+//
+//	printf("I am a simple "); //调用函数
+//	printf("computer.\n");
+//	printf("My favorite number is %d,because it is the first.\n", num);
+//
+//	return 0;
+//
+//}
+
+
+
+
---
+++ 多个函数.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void love(void); //函数声明
+
+int main(void) {
+	printf("hello,");
+	love();  //调用函数
+
+	
+}
+
+void love(void)  //函数定义
+{
+	printf("ly\n");
+}
